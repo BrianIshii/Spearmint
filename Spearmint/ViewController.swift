b47e3fd2b83309ca24a35d2122aad219bdb2716b
Brian Ishii <brianlishii@gmail.com>
2019-04-22 14:57:58 -0700

    Initial Commit

---
+++ Spearmint/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Spearmint
+//
+//  Created by Brian Ishii on 4/22/19.
+//  Copyright © 2019 Brian Ishii. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
