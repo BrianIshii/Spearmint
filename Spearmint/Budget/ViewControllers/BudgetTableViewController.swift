f8f67f1cc33cdfaecbbf48cfc2c663696daef6d5
Brian Ishii <brianlishii@gmail.com>
2019-06-20 23:50:03 -0700

    added section reuse identifier

--- Spearmint/Budget/ViewControllers/BudgetTableViewController.swift
+++ Spearmint/Budget/ViewControllers/BudgetTableViewController.swift
@@ -31,6 +31,7 @@ class BudgetTableViewController: UITableViewController {
         // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
         // self.navigationItem.rightBarButtonItem = self.editButtonItem
         tableView.register(UINib.init(nibName: BudgetItemTableViewCell.xib, bundle: nil), forCellReuseIdentifier: BudgetItemTableViewCell.reuseIdentifier)
+        tableView.register(UINib.init(nibName: BudgetSectionTableViewCell.xib, bundle: nil), forCellReuseIdentifier: BudgetSectionTableViewCell.reuseIdentifier)
         
         dataSource = BudgetDataSource(tableView: tableView)
         
