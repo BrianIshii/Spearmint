52914cf16b19c6e21ddb1ed7f2266a0828153a9a
Brian Ishii <brianlishii@gmail.com>
2019-06-26 23:29:50 -0700

    #2 create default budgets

--- Spearmint/Model/DatabaseAccess/BudgetItemStore.swift
+++ Spearmint/Model/DatabaseAccess/BudgetItemStore.swift
@@ -16,28 +16,24 @@ class BudgetItemStore {
         let dictionary = getBudgetItemDictionary()
         
         if dictionary.count == 0 {
-            activeBudgetItems = defaultBudgetItems()
-        } else {
-            var active = createEmptyBudgetItemDictionary()
-            for (_, budgetItem) in dictionary {
-                if budgetItem.isActive {
-                    if var budgetItems = active[budgetItem.category] {
-                        budgetItems.append(budgetItem)
-                    }
-                }
-            }
-            activeBudgetItems = active
 
+            var dictionary: [String: BudgetItem] = [:]
+            for budgetItem in defaultBudgetItems() {
+                dictionary[budgetItem.id.description()] = budgetItem
+            }
+            budgetItemDictionary = dictionary
+        } else {
+            budgetItemDictionary = dictionary
         }
-        budgetItemDictionary = dictionary
+        activeBudgetItems = getActiveBudgetItems()
     }
 
     public func getBudgetItems() -> [BudgetItemCategory : [BudgetItem]] {
         return activeBudgetItems
     }
     
-    public func getBudgetItem(_ budgetItemID: BudgetItemID) {
-        
+    public func getBudgetItem(_ budgetItemID: BudgetItemID) -> BudgetItem? {
+        return budgetItemDictionary[budgetItemID.description()] ?? nil
     }
     
     public func setActive(_ budgetItem: BudgetItem) {
@@ -57,11 +53,24 @@ class BudgetItemStore {
         return LocalAccess.getDictionary(saveable: BudgetItem.self)
     }
     
+    fileprivate func getActiveBudgetItems() -> [BudgetItemCategory : [BudgetItem]] {
+        var active = createEmptyBudgetItemDictionary()
+        
+        for (_, budgetItem) in budgetItemDictionary {
+            if budgetItem.isActive {
+                if var budgetItems = active[budgetItem.category] {
+                    budgetItems.append(budgetItem)
+                }
+            }
+        }
+        return active
+    }
+    
     fileprivate func updateBudgetItemDictionary() {
         LocalAccess.updateDictionary(data: budgetItemDictionary)
     }
     
-    fileprivate func defaultBudgetItems() -> [BudgetItemCategory:[BudgetItem]] {
+    fileprivate func defaultBudgetItemDictionary() -> [BudgetItemCategory:[BudgetItem]] {
         return [BudgetItemCategory.income : [
             BudgetItem(name: "Paycheck 1", category: BudgetItemCategory.income, planned: 6000)],
                 BudgetItemCategory.housing : [
@@ -97,6 +106,46 @@ class BudgetItemStore {
                     BudgetItem(name: "Credit Card", category: BudgetItemCategory.debt),
                     BudgetItem(name: "Student Loan", category: BudgetItemCategory.debt)],
                 BudgetItemCategory.other : []]
+    }
+    
+    fileprivate func defaultBudgetItems() -> [BudgetItem] {
+        return [
+            // income
+            BudgetItem(name: "Paycheck 1", category: BudgetItemCategory.income, planned: 6000),
+            // housing
+            BudgetItem(name: "Mortgage/Rent", category: BudgetItemCategory.housing, planned: 2500),
+            BudgetItem(name: "Water", category: BudgetItemCategory.housing),
+            BudgetItem(name: "Natural Gas", category: BudgetItemCategory.housing),
+            BudgetItem(name: "Electricity", category: BudgetItemCategory.housing),
+            BudgetItem(name: "Cable", category: BudgetItemCategory.housing),
+            BudgetItem(name: "Internet", category: BudgetItemCategory.housing),
+            BudgetItem(name: "Trash", category: BudgetItemCategory.housing),
+            // transportation
+            BudgetItem(name: "Gas", category: BudgetItemCategory.transportation, planned: 200),
+            BudgetItem(name: "Maintenance", category: BudgetItemCategory.transportation),
+            // giving
+            BudgetItem(name: "Donations", category: BudgetItemCategory.giving),
+            BudgetItem(name: "Gifts", category: BudgetItemCategory.giving),
+            // savings
+            BudgetItem(name: "Retirement", category: BudgetItemCategory.savings, planned: 2000),
+            // food
+            BudgetItem(name: "Groceries", category: BudgetItemCategory.food),
+            BudgetItem(name: "Eating Out", category: BudgetItemCategory.food),
+            // personal
+            BudgetItem(name: "Personal Items", category: BudgetItemCategory.personal, planned: 250),
+            BudgetItem(name: "Subscriptions", category: BudgetItemCategory.personal),
+            // lifestyle
+            BudgetItem(name: "Movies", category: BudgetItemCategory.lifestyle),
+            BudgetItem(name: "Outdoor Stuff", category: BudgetItemCategory.lifestyle),
+            // health
+            BudgetItem(name: "Rock Climbing Gym", category: BudgetItemCategory.health),
+            // insurance
+            BudgetItem(name: "Health Insurance", category: BudgetItemCategory.insurance),
+            BudgetItem(name: "Car Insurance", category: BudgetItemCategory.insurance),
+            // debt
+            BudgetItem(name: "Credit Card", category: BudgetItemCategory.debt),
+            BudgetItem(name: "Student Loan", category: BudgetItemCategory.debt)
+        ]
     }
     
     fileprivate func createEmptyBudgetItemDictionary() -> [BudgetItemCategory:[BudgetItem]] {
