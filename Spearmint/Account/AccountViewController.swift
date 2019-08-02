06b4ab88f34befd440934ec694b385b39d99338c
Brian Ishii <brianlishii@gmail.com>
2019-08-01 23:17:28 -0700

    add cloudkit

--- Spearmint/Account/AccountViewController.swift
+++ Spearmint/Account/AccountViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import CloudKit
 
 class AccountViewController: UIViewController {
 
@@ -34,5 +35,54 @@ class AccountViewController: UIViewController {
         // Pass the selected object to the new view controller.
     }
     */
+    @IBAction func addTransaction(_ sender: Any) {
+        let transaction = Transaction(name: "name", transactionType: TransactionType.expense, vendor: VendorID(), amount: 10.00, date: TransactionDate(), location: "location", image: false, notes: "notes", budgetDate: BudgetDate(), items: [BudgetItemID():[]])
+        
+        let myContainer = CKContainer.default()
+        
+        let privateDatabase = myContainer.privateCloudDatabase
+        
+        let transactionRecordID = CKRecord.ID(recordName: transaction.ID)
+        let transactionRecord = CKRecord(recordType: "Transaction", recordID: transactionRecordID)
+        
+        transactionRecord["name"] = transaction.name as NSString
+        transactionRecord["transactionType"] = transaction.transactionType.rawValue as NSNumber
+        transactionRecord["paymentType"] = transaction.paymentType as NSString
+        transactionRecord["vendorID"] = transaction.vendor.id as NSString
+        transactionRecord["amount"] = transaction.amount as NSNumber
+        transactionRecord["date"] = transaction.date.date as NSDate
+        if transaction.tags.count > 0 {
+            transactionRecord["tags"] = transaction.tags as NSArray
+        }
+        transactionRecord["notes"] = transaction.notes as NSString
+        transactionRecord["amount"] = transaction.amount as NSNumber
+        var budgetItemIDs: [NSString] = []
+        var items: [NSString] = []
+        for (k, v) in transaction.items {
+            budgetItemIDs.append(k.id as NSString)
+            for item in v {
+                items.append(item.budgetItemName as NSString)
+            }
+        }
+        
+        if budgetItemIDs.count > 0 {
+            transactionRecord["budgetItemIDs"] = budgetItemIDs as NSArray
+        }
+        if items.count > 0 {
+            transactionRecord["items"] = items as NSArray
+        }
 
+        privateDatabase.save(transactionRecord) {
+            (record, error) in
+            if let error = error {
+                // Insert error handling
+                print("failed to save: \(error)")
+                return
+            }
+            print("success")
+            print(transaction.ID)
+            // Insert successfully saved record code
+        }
+    }
+    
 }
